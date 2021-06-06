cc3464a0ed9b6058e9c4932f5864c046781c288a
lecongdien2000 <73303456+lecongdien2000@users.noreply.github.com>
2021-06-06 18:39:02 +0700

    update

---
+++ TeacherApplication/app/src/main/java/com/example/teacherapplication/ActivitiesTransfer.java
@@ -0,0 +1,10 @@
+package com.example.teacherapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivitiesTransfer {
+    public static void transfer(Activity fromActivity, Activity toActivity, Bundle bundle){
+
+    }
+}
