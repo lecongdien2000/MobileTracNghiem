da5fc33ed29dcb14119958274d184b448ac4b176
lecongdien2000 <73303456+lecongdien2000@users.noreply.github.com>
2021-06-07 09:10:04 +0700

    update_last

---
+++ TeacherApplication/app/src/main/java/com/example/teacherapplication/LDThongtinActivity.java
@@ -0,0 +1,14 @@
+package com.example.teacherapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class LDThongtinActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lamde_thongtin);
+    }
+}
